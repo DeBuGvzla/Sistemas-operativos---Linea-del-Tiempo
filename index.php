d60ff58c410173acdc871343346cad3da6807530
DeBuGvzla <98239153+DeBuGvzla@users.noreply.github.com>
2024-02-09 17:01:42 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+    <meta name="viewport"="width=device-width, initial-scale=1.0">
+    <title>Línea de Tiempo</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<header>
+    <h3>Sistemas Operativos - Linea del tiempo</h3>
+</header>
+<body>
+    <div class="timeline">
+        <div class="year" id="fecha">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1950<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FMS&nbsp;(Fortran&nbsp;System)</a></div>
+        <div class="year" id="fecha">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1950<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Almacenamiento&nbsp;Temporal</div>
+        <div class="year" id="fecha">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1951<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Monitor&nbsp;Residente</div>
+        <div class="year" id="fecha">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1956<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GM&nbsp;NAA&nbsp;I/O</div>
+        <div class="year" id="fecha">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1961<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sistema&nbsp;Compatible&nbsp;de&nbsp;Tiempo&nbsp;Compartido&nbsp;(CTSS)</div>
+        
+    </div>
+    <div class="info-container">
+        <div class="info" id="info-1950">Es un sistema operativo basado en lotes. Para usarlo, era necesario escribir en papel un programa (o trabajo) en FORTRAN o en ensamblador, luego codificarlo en tarjetas perforadas.</div>
+        <div class="info" id="info-1950">Una memoria temporal, tambien conocida como memoria cache, es un tipo de memoria de acceso rapido que se utiliza para almacenar temporalmente datos o instrucciones que se utilizan con frecuencia en un sistema informatico.</div>
+        <div class="info" id="info-1951">Su funcionamiento era bastante simple, se limitaba a cargar programas a la memoria, leyendolos de una cinta o de tarjetas perforadas y ejecutandolos.</div>
+        <div class="info" id="info-1956">El GM-NAA I/O (de sistema de entrada/salida de General Motors y North American Aviation) es el primer sistema operativo de la historia de la informatica. Fue creado por Robert L. Patrick de General Motors y Owen Mock de North American Aviation para un ordenador IBM 704.</div>
+        <div class="info" id="info-1961">El CTSS, que significa Compatible Time-Sharing System (Sistema de Tiempo Compartido Compatible), fue uno de los primeros sistemas operativos de tiempo compartido; fue desarrollado en el Centro de Computacion del MIT. Se utilizo en el MIT hasta 1973.</div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
